ac1892a79cbc45b6bfc917d46748a3ac11c676a3
MarkvHeusden <markvheusden@hotmail.com>
2021-05-03 13:00:27 +0200

    readme begin

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use((req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Bekijk app op http://localhost:${port}`)
 })
