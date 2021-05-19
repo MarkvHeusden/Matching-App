4926102232ae4d1b2053502a1be8504b5c8e9df1
MarkvHeusden <markvheusden@hotmail.com>
2021-05-19 11:47:15 +0200

    heroku ready

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Middleware & static bestanden
 app.use(express.static('./static/public'))
