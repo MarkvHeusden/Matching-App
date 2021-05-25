8c55335f68f75ad03b9600d651e0794062ec5a51
MarkvHeusden <markvheusden@hotmail.com>
2021-05-25 20:39:19 +0200

    ESLint & Prettier aangepast & connecte Mongo verbeterd

--- server.js
+++ server.js
@@ -5,131 +5,146 @@ const port = process.env.PORT || 3000 // Gebruik Heroku port of localhost port
 
 // MongoDB & .env
 const dotenv = require('dotenv').config()
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
+
+// Maak variabele database aan
+let db
 
 // Verbind met MongoDB
-const dbURI = 'mongodb+srv://mark:FvCp3rZDNjrx9xcU@cluster0.uis9f.mongodb.net/pizza-people?retryWrites=true&w=majority'
-MongoClient.connect(process.env.DB_URI, {
-  useNewUrlParser: true, // Opties object toegevoegd om waarschuwingen in console te voorkomen
-  useUnifiedTopology: true
-}, (err, client) => {
-  if (err) return console.error(err)
-  const db = client.db('pizza-people');
-  const collection = db.collection('personen')
-  const cursor = collection.find({});
-  function iterateFunc(doc) {
-    console.log(JSON.stringify(doc, null, 4));
- }
- 
- cursor.forEach(iterateFunc);
+const connectDB = async () => {
+    const dbURI = process.env.DB_URI
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } // Opties object toegevoegd om waarschuwingen in console te voorkomen
+    const client = new MongoClient(dbURI, options)
+    await client.connect()
+    const db = await client.db(process.env.DB_DBNAME)
+}
 
+connectDB()
+    .then(() => {
+        // Run de server
+        app.listen(port, () => {
+            console.log(`Bekijk app op http://localhost:${port}`)
+        })
 
-  app.listen(port)
-})
+        // const collection = db.collection('personen')
+        // const cursor = collection.find({})
+        // const iterateFunc = (doc) => console.log(JSON.stringify(doc, null, 4))
+
+        // cursor.forEach(iterateFunc)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
 // Middleware & static bestanden
 app.use(express.static('./static/public'))
 app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
 
 // Template engine
 app.set('view engine', 'ejs')
 
 // Tijdelijke data
-let personen = [{
-    id: 52934, // ID om gemakkelijk een detailpagina voor elk account te maken
-    naam: 'Jade',
-    likedMe: true, // likedMe geeft aan of deze persoon mij ook geliket heeft. Zo ja? Dan vind er een match plaats
-    laatsteBericht: "Pizza'tje doen?", // Dit is het laatste chat bericht dat deze persoon heeft verzonden. Is dit leeg dan komt er de tekst te staan dat je een gesprek kunt beginnen.
-    imgFull: '1.jpeg', // Volledige afbeelding voor de detail pagina
-    img: '1.png', // Cropped afbeelding voor de explore en match pagina
-    favPizza: 'Mozzarella',
-    bio: 'Hoi ik ben Jade, en mijn lievelingspizza is mozzarella. In het dagelijks leven studeer ik Creative Business (CB) aan de Hogeschool van Amsterdam. Ik zit momenteel in mijn 3e jaar en ben nu net 22 jaar oud. Verder werk ik in een restaurant als bediening en woon ik in Haarlem. In mijn vrije tijd hou ik mij bezig met gezellig wat drinken met vrienden en natuurlijk pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
-  },
-  {
-    id: 52935,
-    naam: 'Nina',
-    likedMe: true,
-    laatsteBericht: '',
-    imgFull: '2.jpeg',
-    img: '2.jpeg',
-    favPizza: 'Hawaii',
-    bio: 'Nina hier 👋🏼 Mijn lievelingspizza is Hawaii 🥴. In het dagelijks leven studeer ik Verpleegkunde op de Hogeschool Leiden. Ik zit momenteel in mijn 2e jaar en ben nu net 20 jaar oud. Verder werk ik als clown in het circus en woon ik in Leiden. In mijn vrije tijd hou ik mij bezig met sporten en natuurlijk af en toe een pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
-  },
-  {
-    id: 52936,
-    naam: 'Lisa',
-    likedMe: false,
-    laatsteBericht: '',
-    imgFull: '3.jpeg',
-    img: '3.jpeg',
-    favPizza: 'Salami',
-    bio: 'Hey daar! Ik ben Lisa en pizza salami is mijn absolute favoriet! In het dagelijks leven studeer ik Ondernemerschap & Retail Management op de Haagse Hogeschool. Ik zit momenteel in mijn 1e jaar en ben nu net 20 jaar oud. Verder werk ik in een restaurant als bezorger en woon ik in in Leidschendam. In mijn vrije tijd hou ik mij bezig met puzzelen en natuurlijk pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
-  }
+const personen = [
+    {
+        id: 52934, // ID om gemakkelijk een detailpagina voor elk account te maken
+        naam: 'Jade',
+        likedMe: true, // likedMe geeft aan of deze persoon mij ook geliket heeft. Zo ja? Dan vind er een match plaats
+        laatsteBericht: "Pizza'tje doen?", // Dit is het laatste chat bericht dat deze persoon heeft verzonden. Is dit leeg dan komt er de tekst te staan dat je een gesprek kunt beginnen.
+        imgFull: '1.jpeg', // Volledige afbeelding voor de detail pagina
+        img: '1.png', // Cropped afbeelding voor de explore en match pagina
+        favPizza: 'Mozzarella',
+        bio: 'Hoi ik ben Jade, en mijn lievelingspizza is mozzarella. In het dagelijks leven studeer ik Creative Business (CB) aan de Hogeschool van Amsterdam. Ik zit momenteel in mijn 3e jaar en ben nu net 22 jaar oud. Verder werk ik in een restaurant als bediening en woon ik in Haarlem. In mijn vrije tijd hou ik mij bezig met gezellig wat drinken met vrienden en natuurlijk pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
+    },
+    {
+        id: 52935,
+        naam: 'Nina',
+        likedMe: true,
+        laatsteBericht: '',
+        imgFull: '2.jpeg',
+        img: '2.jpeg',
+        favPizza: 'Hawaii',
+        bio: 'Nina hier 👋🏼 Mijn lievelingspizza is Hawaii 🥴. In het dagelijks leven studeer ik Verpleegkunde op de Hogeschool Leiden. Ik zit momenteel in mijn 2e jaar en ben nu net 20 jaar oud. Verder werk ik als clown in het circus en woon ik in Leiden. In mijn vrije tijd hou ik mij bezig met sporten en natuurlijk af en toe een pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
+    },
+    {
+        id: 52936,
+        naam: 'Lisa',
+        likedMe: false,
+        laatsteBericht: '',
+        imgFull: '3.jpeg',
+        img: '3.jpeg',
+        favPizza: 'Salami',
+        bio: 'Hey daar! Ik ben Lisa en pizza salami is mijn absolute favoriet! In het dagelijks leven studeer ik Ondernemerschap & Retail Management op de Haagse Hogeschool. Ik zit momenteel in mijn 1e jaar en ben nu net 20 jaar oud. Verder werk ik in een restaurant als bezorger en woon ik in in Leidschendam. In mijn vrije tijd hou ik mij bezig met puzzelen en natuurlijk pizza eten. Like mij als je opzoek bent naar een pizza maatje.'
+    }
 ]
 
-let huidigePersoon = personen.find(huidigePersoon => huidigePersoon) // Persoon die je nu aan het beoordelen bent -> persoon bovenaan personen die je moet beoordelen
-let matches = [] // Array voor toekomstige matched
-let likedPersonen = [] // Array voor toekomstige gelikete personen
-let skippedPersonen = [] // Array voor toekomstige geskipte personen
+let huidigePersoon = personen.find((huidigePersoon) => huidigePersoon) // Persoon die je nu aan het beoordelen bent -> persoon bovenaan personen die je moet beoordelen
+const matches = [] // Array voor toekomstige matched
+const likedPersonen = [] // Array voor toekomstige gelikete personen
+const skippedPersonen = [] // Array voor toekomstige geskipte personen
 
 // Routes
 app.get('/', (req, res) => {
-  res.redirect('/explore') // Stuur mensen door naar de explore pagina
+    res.redirect('/explore') // Stuur mensen door naar de explore pagina
 })
 
 app.get('/explore', (req, res) => {
-  res.render('explore', {
-    title: 'Ontdek personen',
-    huidigePersoon
-  })
+    res.render('explore', {
+        title: 'Ontdek personen',
+        huidigePersoon
+    })
 })
 
 app.get('/explore/:persoonId', (req, res) => {
-  const persoon = personen.find(persoon => persoon.id === parseInt(req.params.persoonId, 10)) // Vind de persoon op basis van het id dat in de URL staat
-  res.render('details', {
-    title: persoon.naam,
-    persoon
-  })
+    const persoon = personen.find(
+        (persoon) => persoon.id === parseInt(req.params.persoonId, 10)
+    ) // Vind de persoon op basis van het id dat in de URL staat
+    res.render('details', {
+        title: persoon.naam,
+        persoon
+    })
 })
 
 app.get('/matches', (req, res) => {
-  res.render('matches', {
-    title: 'Mijn matches',
-    matches
-  })
+    res.render('matches', {
+        title: 'Mijn matches',
+        matches
+    })
 })
 
 app.get('/account', (req, res) => {
-  res.render('account', {
-    title: 'Mijn account'
-  })
+    res.render('account', {
+        title: 'Mijn account'
+    })
 })
 
 // Like / skip
-app.post(['/explore', '/explore/:persoonId'], (req, res) => { // Formulier op zowel de ontdek als detailpagina
-  if (req.body.rate === 'like') { // Als like knop is gedrukt
-    likedPersonen.push(huidigePersoon) // Voeg dan deze persoon toe aan de likedPersonen array
-    personen.shift() // En verwijder deze persoon uit de personen die je nog moet beoordelen
-    if (huidigePersoon.likedMe) { // Als deze persoon mij ook heeft geliked
-      matches.push(huidigePersoon) // Maak dan een match aan
+app.post(['/explore', '/explore/:persoonId'], (req, res) => {
+    // Formulier op zowel de ontdek als detailpagina
+    if (req.body.rate === 'like') {
+        // Als like knop is gedrukt
+        likedPersonen.push(huidigePersoon) // Voeg dan deze persoon toe aan de likedPersonen array
+        personen.shift() // En verwijder deze persoon uit de personen die je nog moet beoordelen
+        if (huidigePersoon.likedMe) {
+            // Als deze persoon mij ook heeft geliked
+            matches.push(huidigePersoon) // Maak dan een match aan
+        }
+    } else if (req.body.rate === 'skip') {
+        skippedPersonen.push(huidigePersoon) // Als skip knop is gedrukt
+        personen.shift() // Verwijder deze persoon uit de personen die je nog moet beoordelen
     }
-  } else if (req.body.rate === 'skip') {
-    skippedPersonen.push(huidigePersoon) // Als skip knop is gedrukt
-    personen.shift() // Verwijder deze persoon uit de personen die je nog moet beoordelen
-  }
-  huidigePersoon = personen.find(huidigePersoon => huidigePersoon) // Update de huidige persoon naar een nieuwe persoon
-  res.redirect('/explore') // Stuur mensen door naar de explore pagina. Geen render hier omdat mensen van de detail pagina dan deze URL met id behouden waardoor de nav bar niet meer ziet dat ze op de explore pagina zijn.
+    huidigePersoon = personen.find((huidigePersoon) => huidigePersoon) // Update de huidige persoon naar een nieuwe persoon
+    res.redirect('/explore') // Stuur mensen door naar de explore pagina. Geen render hier omdat mensen van de detail pagina dan deze URL met id behouden waardoor de nav bar niet meer ziet dat ze op de explore pagina zijn.
 })
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).render('404')
+    res.status(404).render('404')
 })
-
-// Run de server lokaal
-// app.listen(port, () => {
-//   console.log(`Bekijk app op http://localhost:${port}`)
-// })
--- static/public/js/script.js
+++ static/public/js/script.js
@@ -3,7 +3,7 @@ const huidigeURL = location.href
 const headerItems = document.querySelectorAll('header nav a')
 
 // Voor elk header item kijken of het overeenkomt met de huidige URL
-headerItems.forEach(headerItem => {
+headerItems.forEach((headerItem) => {
     if (headerItem.href === huidigeURL) {
         headerItem.classList.add('active')
     }
